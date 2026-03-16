855ad84fd72965f04985e4b279b80b74a4f851ed
Naga Brahma Chary <nagachary018@gmail.com>
2026-03-16 06:09:25 -0700

    added  requirement-analysis-agent module

---
+++ requirement-analysis-agent/src/main/java/com/naga/ai/RequirementAnalysisAgentApplication.java
@@ -0,0 +1,13 @@
+package com.naga.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementAnalysisAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequirementAnalysisAgentApplication.class, args);
+    }
+
+}
